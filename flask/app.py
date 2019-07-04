27707ad28c76bc97c43bed369268431ebacc237e
Tavernier Antoine <tavernier.antoine@hotmail.fr>
2019-07-04 15:35:50 +0200

    [FLASK] OKish

--- flask/app.py
+++ flask/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template, send_from_directory
+from flask import Flask, request, render_template, send_from_directory, url_for
 import os
 import sys
 from flask_material import Material  
@@ -13,18 +13,17 @@ def index():
     print("test")
     return render_template('index.html')
 
-@app.route('/handle_movie/', methods=['GET', 'POST'])
+@app.route('/handle_movie/', methods=['POST'])
 def handle_movie():
-    print("HELOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO")
     movie_name = request.form['movie']
     print(movie_name)
-    return str(movie_name)
+    return render_template("result.html")
 
 @app.route('/handle_actor/', methods=['POST'])
 def handle_actor():
     actor_name = request.form['actor']
-
-    return render_template("index.html")
+    print(actor_name)
+    return render_template("result.html")
     
 
 if __name__ == "__main__":

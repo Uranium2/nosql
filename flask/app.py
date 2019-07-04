6249a463216c9e8e1115c14a4266d18742d8bbf8
Tavernier Antoine <tavernier.antoine@hotmail.fr>
2019-07-04 12:40:51 +0200

    [FLASK] materialize

--- flask/app.py
+++ flask/app.py
@@ -1,9 +1,29 @@
-from flask import Flask
+from flask import Flask, request, render_template, send_from_directory
+import os
+import sys
+from flask_material import Material  
+
 app = Flask(__name__)
+Material(app)
+
+APP_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 @app.route('/')
-def hello_world():
-    return 'Hello, Worereld!'
+def index():
+    return render_template('index.html')
+
+@app.route('/handle_movie', methods=['POST'])
+def handle_movie():
+    movie_name = request.form['movie']
+
+    return render_template("index.html")
+
+@app.route('/handle_actor', methods=['POST'])
+def handle_actor():
+    actor_name = request.form['actor']
+
+    return render_template("index.html")
+    
 
 if __name__ == "__main__":
     app.run(debug=False, host= '0.0.0.0')
